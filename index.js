2b786e1a5c32cfcee6a0eba23266f87d3f4fc2ea
Zwavhu <zwavhudimulelu@gmail.com>
2020-11-08 09:38:24 +0200

    routing and cdn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const appConstants = require('./appConstants')
 const mainRouter =  require('./mainRoutes.js')
 
 app.use('/', mainRouter)
+app.use('/cdn', express.static('public'))
+
+// serving static files
+app.use('/cdn', express.static('public'))
 
 app.listen(appConstants.APP_PORT)
 console.log(`Server listening on port:  ${appConstants.APP_PORT}`)
--- mainRoutes.js
+++ mainRoutes.js
@@ -8,4 +8,12 @@ mainRouter.get('/', function (req, res){
     res.sendFile(path.join(__dirname, 'views', 'welcome.html'))
 })
 
+mainRouter.get('/survey', function (req, res){
+    res.sendFile(path.join(__dirname, 'views', 'survey.html'))
+})
+
+mainRouter.get('/results', function (req, res){
+    res.sendFile(path.join(__dirname, 'views', 'results.html'))
+})
+
 module.exports = mainRouter
